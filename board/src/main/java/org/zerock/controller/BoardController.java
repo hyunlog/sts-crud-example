bd68af48ab6a288cdc74e32e02f0b8359477466f
hyunlog <hh_08@naver.com>
2025-06-09 10:15:07 +0900

    implement JSP CRUD

--- board/src/main/java/org/zerock/controller/BoardController.java
+++ board/src/main/java/org/zerock/controller/BoardController.java
@@ -28,6 +28,11 @@ public class BoardController {
 		model.addAttribute("list", service.getList());
 	}
 	
+	@GetMapping("/register") 
+	public void register() {
+		
+	}
+	
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		
@@ -39,9 +44,9 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 	
-	@GetMapping("/get")
+	@GetMapping({"/get", "/modify"})
 	public void get(@RequestParam("bno") Long bno, Model model) {
-		log.info("------------------------------- get");
+		log.info("------------------------------- get or modify");
 		model.addAttribute("board", service.get(bno));
 	}
 	
